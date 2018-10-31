3bbede9073b12a0f42433fec3e9d283b92c503ab
luhibe <5393363@qq.com>
2018-10-31 20:11:32 +0800

    第九课 cin语句

---
+++ ex009-1.cpp
@@ -0,0 +1,17 @@
+/*	卢海北学c++ ex009-1.cpp 2018.10.31 
+	
+	从键盘输入长方形的长和宽，输出长方形的周长。 
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+	int a,b,c;
+	cin>>a>>b;
+	c=(a+b)*2;
+	cout<<"周长："<<c<<endl;	
+	return 0;
+}
